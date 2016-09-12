294c4302c4938acb5a8ca1961f3fd9cda1675eaf
Demnart <insidia93@gmail.com>
2016-09-13 00:48:08 +0300

    Пробная версия

---
+++ src/SeabattleLine.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * Created by Артем on 12.09.2016.
+ */
+public class SeabattleLine {
+    public static void main(String[] args) {
+        char[] cells = new char[10];
+
+        for (int i = 0; i < cells.length; i++) {
+            cells[i] = '.';
+        }
+        int position = 4;
+        cells[position] = 'X';
+        do {
+            System.out.println(cells);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите координаты вашего выстрела");
+            String s;
+            s = scanner.nextLine();
+            System.out.println("Координаты подтверждены");
+            int shoot = Integer.parseInt(s);
+            switch (cells[shoot]) {
+                case '.':
+                    System.out.println("Вы промахнулись");
+                    cells[shoot] = '*';
+                    break;
+                case 'X':
+                    System.out.println("Вы попали!!!");
+                    cells[shoot] = '$';
+                    System.out.println(cells);
+                    break;
+                case '*':
+                    System.out.println("Вы уже стреляли в эту ячейку");
+                    break;
+                default:
+                    System.out.println("Eror");
+            }
+
+        } while (cells[position] == 'X');
+
+
+
+
+
+
+
+    }
+
+}
