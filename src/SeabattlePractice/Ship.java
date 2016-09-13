a7e05b7471d8a8eb60e2fd1d8c60eabc5193d8e7
Demnart <insidia93@gmail.com>
2016-09-13 15:44:34 +0300

    Создан класс корабля и у него появилась переменная позиции

---
+++ src/SeabattlePractice/Ship.java
@@ -0,0 +1,12 @@
+package SeabattlePractice;
+
+/**
+ * Created by Артем on 13.09.2016.
+ */
+public class Ship {
+    int position;
+
+    public Ship(int position) {
+        this.position = position;
+    }
+}
